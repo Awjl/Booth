5623a2170fb483f758a1e0d3546c8a2681408cef
佳乐 <le179516844@163.com>
2019-01-02 13:43:42 +0800

    登录注册

---
+++ src/components/sign/sign.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="sign">注册</div>
+</template>
+
+<script>
+export default {
+  name: "sign"
+};
+</script>
--- src/views/all.vue
+++ src/views/all.vue
@@ -3,23 +3,24 @@
     <Tab></Tab>
     <router-view></router-view>
     <Food></Food>
+    <Login></Login>
+    <Sign></Sign>
   </div>
 </template>
 
 <script>
 import Tab from "@/components/tab/tab.vue";
 import Food from "@/components/food/food.vue";
+import Login from "@/components/login/login.vue";
+import Sign from "@/components/sign/sign.vue";
 
 export default {
   name: "all",
   components: {
     Tab,
-    Food
+    Food,
+    Login,
+    Sign
   }
 };
 </script>
-
-<style lang="scss">
-.all {
-}
-</style>

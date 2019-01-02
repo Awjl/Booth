4a29822ed8a1aa3328dddb9e1e5f3cb36c07e07a
佳乐 <le179516844@163.com>
2019-01-02 17:18:09 +0800

    搜索页面

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -25,13 +25,116 @@
     </div>
     <div class="searchAll">
       <div class="searchMine">
-        <div class="searchMineTop"></div>
-        <div class="searchMineBottom"></div>
+        <div class="searchMineTop">
+          <div class="searchTitle">
+            <span>展会 exhibition</span>
+            <div class="mover"><i class="icon iconTo1"></i></div>
+          </div>
+          <div class="exhibitionList">
+            <div class="exhibitionItem">
+              <div class="exhibitionItemImg">
+                <img src="../../assets/images/search/one.png" alt="" />
+              </div>
+              <div class="exhibitionItemName">
+                HOUSE VISION 2018 BEIJING EXHIBITION 探索家——未来生活大展
+              </div>
+            </div>
+            <div class="exhibitionItem">
+              <div class="exhibitionItemImg">
+                <img src="../../assets/images/search/one.png" alt="" />
+              </div>
+              <div class="exhibitionItemName">
+                HOUSE VISION 2018 BEIJING EXHIBITION 探索家——未来生活大展
+              </div>
+            </div>
+            <div class="exhibitionItem">
+              <div class="exhibitionItemImg">
+                <img src="../../assets/images/search/one.png" alt="" />
+              </div>
+              <div class="exhibitionItemName">
+                HOUSE VISION 2018 BEIJING EXHIBITION 探索家——未来生活大展
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="searchMineBottom">
+          <div class="searchTitle">
+            <span>产品手册 brochure</span>
+            <div class="mover"><i class="icon iconTo1"></i></div>
+          </div>
+          <div class="brochureList">
+            <div class="brochureItem">
+              <div class="brochureItemImg"></div>
+              <div class="brochureItemText">
+                <div class="brochureItemHead">
+                  <div class="brochureItemName">
+                    <div class="brochureItemLogo">
+                      <img src="../../assets/images/home/head2.png" alt="" />
+                    </div>
+                    <div class="brochureLogoName">
+                      <p>某某公司企业宣传手册</p>
+                      <p>Sichuan，Chengdu 856关注者</p>
+                      <p>建筑设计行业</p>
+                    </div>
+                  </div>
+                  <div class="brochureItemNum">阅读量 3000+</div>
+                </div>
+                <div class="brochureItemmanual">
+                  <p>某某公司企业宣传手册</p>
+                  <div class="label">
+                    <span>标签</span><span>标签</span><span>标签</span>
+                  </div>
+                  <div class="brochureintroduce"></div>
+                </div>
+              </div>
+            </div>
+            <div class="brochureItem">
+              <div class="brochureItemImg"></div>
+              <div class="brochureItemText">
+                <div class="brochureItemHead">
+                  <div class="brochureItemName">
+                    <div class="brochureItemLogo">
+                      <img src="../../assets/images/home/head2.png" alt="" />
+                    </div>
+                    <div class="brochureLogoName">
+                      <p>某某公司企业宣传手册</p>
+                      <p>Sichuan，Chengdu 856关注者</p>
+                      <p>建筑设计行业</p>
+                    </div>
+                  </div>
+                  <div class="brochureItemNum">阅读量 3000+</div>
+                </div>
+                <div class="brochureItemmanual">
+                  <p>某某公司企业宣传手册</p>
+                  <div class="label">
+                    <span>标签</span><span>标签</span><span>标签</span>
+                  </div>
+                  <div class="brochureintroduce"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="searchRight">
-        <div class="searchRightTop"></div>
-        <div class="searchRightmian"></div>
-        <div class="searchRightBottom"></div>
+        <div class="searchRightTop">
+          <div class="searchTitle">
+            <span>企业 company</span>
+            <div class="mover"><i class="icon iconTo1"></i></div>
+          </div>
+        </div>
+        <div class="searchRightmian">
+          <div class="searchTitle">
+            <span>产品图片 images</span>
+            <div class="mover"><i class="icon iconTo1"></i></div>
+          </div>
+        </div>
+        <div class="searchRightBottom">
+          <div class="searchTitle">
+            <span>企业动态 event</span>
+            <div class="mover"><i class="icon iconTo1"></i></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -80,6 +183,26 @@ export default {
     width: calc(100% - 184px);
     height: 100%;
     display: flex;
+    .searchTitle {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 10px;
+      span {
+        font-size: 18px;
+        font-weight: bold;
+      }
+      .mover {
+        width: 24px;
+        height: 24px;
+        background: #326b90;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+      }
+    }
     .searchMine {
       width: calc(50% - 10px);
       height: 100%;
@@ -89,13 +212,103 @@ export default {
         height: calc(45% - 10px);
         background: #f2f2f2;
         margin-bottom: 10px;
+        padding: 16px 24px 0;
+        box-sizing: border-box;
         overflow: auto;
+        .exhibitionItem {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-bottom: 10px;
+          .exhibitionItemImg {
+            width: 45%;
+            img {
+              width: 100%;
+            }
+          }
+          .exhibitionItemName {
+            width: 35%;
+            line-height: 26px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 24px;
+          }
+        }
       }
       .searchMineBottom {
         width: 100%;
         height: 55%;
         background: #f2f2f2;
+        padding: 16px 24px 0;
+        box-sizing: border-box;
         overflow: auto;
+        .brochureItem {
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 10px;
+          .brochureItemImg {
+            width: 162px;
+            background: #fff;
+          }
+          .brochureItemText {
+            width: calc(100% - 182px);
+            margin-left: 20px;
+            background: #f2f2f2;
+            .brochureItemName {
+              display: flex;
+              .brochureLogoName {
+                margin-left: 10px;
+                p:nth-child(1) {
+                  margin-top: 4px;
+                  font-size: 16px;
+                  font-weight: bold;
+                }
+                p:nth-child(2) {
+                  margin-top: 6px;
+                }
+                p:nth-child(3) {
+                  color: #326b90;
+                  font-weight: bold;
+                  font-size: 12px;
+                  margin-top: 6px;
+                }
+              }
+            }
+            .brochureItemHead {
+              display: flex;
+              justify-content: space-between;
+              .brochureItemNum {
+                font-weight: bold;
+                margin-top: 30px;
+              }
+            }
+            .brochureItemmanual {
+              p {
+                font-size: 16px;
+                margin: 6px 0;
+              }
+              .label {
+                span {
+                  display: inline-block;
+                  width: 60px;
+                  height: 24px;
+                  text-align: center;
+                  line-height: 24px;
+                  margin-right: 4px;
+                  background: rgba($color: #000000, $alpha: 0.2);
+                  color: #fff;
+                }
+              }
+              .brochureintroduce {
+                width: 100%;
+                height: 64px;
+                background: rgba($color: #000000, $alpha: 0.2);
+                margin-top: 10px;
+              }
+            }
+          }
+        }
       }
     }
     .searchRight {
@@ -107,6 +320,8 @@ export default {
         height: calc(35% - 10px);
         background: #f2f2f2;
         margin-bottom: 10px;
+        padding: 16px 24px 0;
+        box-sizing: border-box;
         overflow: auto;
       }
       .searchRightmian {
@@ -114,12 +329,16 @@ export default {
         height: calc(40% - 10px);
         background: #f2f2f2;
         margin-bottom: 10px;
+        padding: 16px 24px 0;
+        box-sizing: border-box;
         overflow: auto;
       }
       .searchRightBottom {
         width: 100%;
         height: 25%;
         background: #f2f2f2;
+        padding: 16px 24px 0;
+        box-sizing: border-box;
         overflow: auto;
       }
     }
